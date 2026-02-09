f4ccbbb143738046d40c55795e123a974b6d0ada
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-09 20:39:25 +0100

    feat: update AuctionsTable to use ItemStack serialization for item storage

--- surf-shop-backend/src/main/kotlin/dev/slne/surf/shop/backend/table/AuctionsTable.kt
+++ surf-shop-backend/src/main/kotlin/dev/slne/surf/shop/backend/table/AuctionsTable.kt
@@ -2,12 +2,18 @@ package dev.slne.surf.shop.backend.table
 
 import dev.slne.surf.database.columns.nativeUuid
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.dao.id.ULongIdTable
+import org.bukkit.inventory.ItemStack
 
 object AuctionsTable : ULongIdTable("shop_auctions") {
     val auctionUuid = nativeUuid("auction_uuid").uniqueIndex()
-    val item = binary("item")
+    val item = binary("item_stack").transform(
+        { itemStackFromString(it) },
+        { itemStackToString(it) })
     val storedItemCount = integer("item_count")
     val pricePerItem = integer("price_per_item")
     val seller = nativeUuid("seller")
     val createdAt = long("created_at")
 }
+
+fun itemStackToString(itemStack: ItemStack): ByteArray = itemStack.serializeAsBytes()
+fun itemStackFromString(encoded: ByteArray): ItemStack = ItemStack.deserializeBytes(encoded)
