13be0bbcbc4ba9aed10419781311a9750187e443
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-10 08:53:24 +0100

    feat: implement auction command to open AuctionListView for players

--- surf-shop-paper/src/main/kotlin/dev/slne/surf/shop/paper/PaperMain.kt
+++ surf-shop-paper/src/main/kotlin/dev/slne/surf/shop/paper/PaperMain.kt
@@ -3,6 +3,7 @@ package dev.slne.surf.shop.paper
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
 import dev.slne.surf.shop.core.service.auctionService
 import dev.slne.surf.shop.core.service.dealService
+import dev.slne.surf.shop.paper.command.auctionCommand
 import org.bukkit.plugin.java.JavaPlugin
 
 val plugin get() = JavaPlugin.getPlugin(PaperMain::class.java)
@@ -11,5 +12,9 @@ class PaperMain : SuspendingJavaPlugin() {
     override suspend fun onLoadAsync() {
         auctionService.fetchAuctions()
         dealService.fetchDeals()
+    }
+
+    override suspend fun onEnableAsync() {
+        auctionCommand()
     }
 }
--- surf-shop-paper/src/main/kotlin/dev/slne/surf/shop/paper/command/AuctionCommand.kt
+++ surf-shop-paper/src/main/kotlin/dev/slne/surf/shop/paper/command/AuctionCommand.kt
@@ -1,8 +1,15 @@
 package dev.slne.surf.shop.paper.command
 
 import dev.jorel.commandapi.kotlindsl.commandTree
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import dev.slne.surf.shop.paper.menu.AuctionListView
 import dev.slne.surf.shop.paper.permission.PermissionRegistry
+import dev.slne.surf.surfapi.bukkit.api.inventory.framework.viewFrame
 
 fun auctionCommand() = commandTree("auction") {
     withPermission(PermissionRegistry.AUCTION_COMMAND)
+
+    playerExecutor { player, _ ->
+        viewFrame.open(AuctionListView::class.java, player)
+    }
 }
