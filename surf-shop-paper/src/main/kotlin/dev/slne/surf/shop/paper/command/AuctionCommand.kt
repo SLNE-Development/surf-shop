f486203ef2e7ac6b2fd6a43c488176ebab69410a
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-11 19:07:26 +0100

    feat: add search argument handling for auction command

--- surf-shop-paper/src/main/kotlin/dev/slne/surf/shop/paper/command/AuctionCommand.kt
+++ surf-shop-paper/src/main/kotlin/dev/slne/surf/shop/paper/command/AuctionCommand.kt
@@ -1,6 +1,8 @@
 package dev.slne.surf.shop.paper.command
 
 import dev.jorel.commandapi.kotlindsl.commandTree
+import dev.jorel.commandapi.kotlindsl.getValue
+import dev.jorel.commandapi.kotlindsl.greedyStringArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 import dev.slne.surf.shop.paper.menu.AuctionListView
 import dev.slne.surf.shop.paper.permission.PermissionRegistry
@@ -16,5 +18,16 @@ fun auctionCommand() = commandTree("auction") {
             AuctionListView::class.java,
             player
         )
+
+        greedyStringArgument("search") {
+            playerExecutor { player, args ->
+                val search: String by args
+                searchInputCache[player.uniqueId] = search
+                viewFrame.open(
+                    AuctionListView::class.java,
+                    player
+                )
+            }
+        }
     }
 }
