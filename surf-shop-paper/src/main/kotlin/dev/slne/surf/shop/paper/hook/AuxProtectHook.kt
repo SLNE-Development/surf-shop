1e04bd7a2c1e226b83c62f39dea290da7ae93f28
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-06 17:38:34 +0100

    feat: implement asynchronous logging for shop actions in AuxProtectHook

--- surf-shop-paper/src/main/kotlin/dev/slne/surf/shop/paper/hook/AuxProtectHook.kt
+++ surf-shop-paper/src/main/kotlin/dev/slne/surf/shop/paper/hook/AuxProtectHook.kt
@@ -6,6 +6,7 @@ import dev.heliosares.auxprotect.database.EntryAction
 import dev.heliosares.auxprotect.spigot.AuxProtectSpigot
 import dev.slne.surf.shop.api.shop.Shop
 import dev.slne.surf.shop.paper.plugin
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 object AuxProtectHook {
@@ -16,79 +17,97 @@ object AuxProtectHook {
     lateinit var shopWithdrawnAction: EntryAction
 
     fun create() {
-        // @formatter:off
-        shopCreatedAction = AuxProtectAPI.createAction(plugin.name, "surfshop_created", "created shop", null)
-        shopDeletedAction = AuxProtectAPI.createAction(plugin.name, "surfshop_deleted", "deleted shop", null)
-        shopBoughtAction = AuxProtectAPI.createAction(plugin.name, "surfshop_bought", "bought from shop", null)
-        shopDepositedAction = AuxProtectAPI.createAction(plugin.name, "surfshop_deposited", "deposited items into shop", null)
-        shopWithdrawnAction = AuxProtectAPI.createAction(plugin.name, "surfshop_withdrawn", "withdrew items from shop", null)
-        // @formatter:on
+        Bukkit.getAsyncScheduler().runNow(plugin) {
+            runCatching {
+                // @formatter:off
+                shopCreatedAction = AuxProtectAPI.createAction(plugin.name, "surfshop_created", "created shop", null)
+                shopDeletedAction = AuxProtectAPI.createAction(plugin.name, "surfshop_deleted", "deleted shop", null)
+                shopBoughtAction = AuxProtectAPI.createAction(plugin.name, "surfshop_bought", "bought from shop", null)
+                shopDepositedAction = AuxProtectAPI.createAction(plugin.name, "surfshop_deposited", "deposited items into shop", null)
+                shopWithdrawnAction = AuxProtectAPI.createAction(plugin.name, "surfshop_withdrawn", "withdrew items from shop", null)
+                // @formatter:on
+            }
 
-        plugin.logger.info("Hooked into AuxProtect. Creations, deletions, buys, deposits and withdrawals will now be logged.")
+            plugin.logger.info("Hooked into AuxProtect. Creations, deletions, buys, deposits and withdrawals will now be logged.")
+        }
     }
 
+    private fun createAction(name: String, key: String, nText: String): EntryAction = runCatching {
+        AuxProtectAPI.createAction(name, key, nText, null)
+    }.getOrNull() ?: EntryAction.getAction(key)
+
     fun logBuy(player: Player, shop: Shop, amount: Int) {
-        AuxProtectAPI.add(
-            DbEntry(
-                AuxProtectSpigot.getLabel(player),
-                shopBoughtAction,
-                true,
-                null,
-                "#shop",
-                "shopOwner=${shop.seller}; amount=$amount; pricePerItem=${shop.pricePerItem}; item=${shop.item.serialize()} "
+        Bukkit.getAsyncScheduler().runNow(plugin) {
+            AuxProtectAPI.add(
+                DbEntry(
+                    AuxProtectSpigot.getLabel(player),
+                    shopBoughtAction,
+                    true,
+                    null,
+                    "#shop",
+                    "shopOwner=${shop.seller}; amount=$amount; pricePerItem=${shop.pricePerItem}; item=${shop.item.serialize()} "
+                )
             )
-        )
+        }
     }
 
     fun logCreate(player: Player, shop: Shop) {
-        AuxProtectAPI.add(
-            DbEntry(
-                AuxProtectSpigot.getLabel(player),
-                shopCreatedAction,
-                true,
-                null,
-                "#shop",
-                "shopOwner=${shop.seller}; item=${shop.item.serialize()}"
+        Bukkit.getAsyncScheduler().runNow(plugin) {
+            AuxProtectAPI.add(
+                DbEntry(
+                    AuxProtectSpigot.getLabel(player),
+                    shopCreatedAction,
+                    true,
+                    null,
+                    "#shop",
+                    "shopOwner=${shop.seller}; item=${shop.item.serialize()}"
+                )
             )
-        )
+        }
     }
 
     fun logDelete(player: Player, shop: Shop) {
-        AuxProtectAPI.add(
-            DbEntry(
-                AuxProtectSpigot.getLabel(player),
-                shopDeletedAction,
-                true,
-                null,
-                "#shop",
-                "shopOwner=${shop.seller}; item=${shop.item.serialize()}"
+        Bukkit.getAsyncScheduler().runNow(plugin) {
+            AuxProtectAPI.add(
+                DbEntry(
+                    AuxProtectSpigot.getLabel(player),
+                    shopDeletedAction,
+                    true,
+                    null,
+                    "#shop",
+                    "shopOwner=${shop.seller}; item=${shop.item.serialize()}"
+                )
             )
-        )
+        }
     }
 
     fun logDeposit(player: Player, shop: Shop, amount: Int) {
-        AuxProtectAPI.add(
-            DbEntry(
-                AuxProtectSpigot.getLabel(player),
-                shopDepositedAction,
-                true,
-                null,
-                "#shop",
-                "shopOwner=${shop.seller}; amount=$amount; item=${shop.item.serialize()}"
+        Bukkit.getAsyncScheduler().runNow(plugin) {
+            AuxProtectAPI.add(
+                DbEntry(
+                    AuxProtectSpigot.getLabel(player),
+                    shopDepositedAction,
+                    true,
+                    null,
+                    "#shop",
+                    "shopOwner=${shop.seller}; amount=$amount; item=${shop.item.serialize()}"
+                )
             )
-        )
+        }
     }
 
     fun logWithdraw(player: Player, shop: Shop, amount: Int) {
-        AuxProtectAPI.add(
-            DbEntry(
-                AuxProtectSpigot.getLabel(player),
-                shopWithdrawnAction,
-                true,
-                null,
-                "#shop",
-                "shopOwner=${shop.seller}; amount=$amount; item=${shop.item.serialize()}"
+        Bukkit.getAsyncScheduler().runNow(plugin) {
+            AuxProtectAPI.add(
+                DbEntry(
+                    AuxProtectSpigot.getLabel(player),
+                    shopWithdrawnAction,
+                    true,
+                    null,
+                    "#shop",
+                    "shopOwner=${shop.seller}; amount=$amount; item=${shop.item.serialize()}"
+                )
             )
-        )
+        }
     }
 }
