41f19434738e9ced537a9c22399794c4a3643ab6
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-14 10:59:57 +0100

    feat: update shopInternalId reference in DealsTable to use CASCADE option

--- surf-shop-backend/src/main/kotlin/dev/slne/surf/shop/backend/table/DealsTable.kt
+++ surf-shop-backend/src/main/kotlin/dev/slne/surf/shop/backend/table/DealsTable.kt
@@ -2,11 +2,13 @@ package dev.slne.surf.shop.backend.table
 
 import dev.slne.surf.database.columns.nativeUuid
 import dev.slne.surf.database.columns.time.offsetDateTime
+import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.ReferenceOption
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.dao.id.ULongIdTable
 
 object DealsTable : ULongIdTable("shop_deals") {
     val dealUuid = nativeUuid("deal_uuid").uniqueIndex()
-    val shopInternalId = ulong("shop_internal_id").references(ShopsTable.id)
+    val shopInternalId =
+        ulong("shop_internal_id").references(ShopsTable.id, ReferenceOption.CASCADE)
     val amount = integer("amount")
     val boughtBy = nativeUuid("bought_by")
     val boughtAt = offsetDateTime("bought_at")
